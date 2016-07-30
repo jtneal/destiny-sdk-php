dc6207f46e345a5aa060c7949dbf8a50943c17bb
Jason Neal <jtneal@gmail.com>
2016-07-29 21:53:00 -0400

    Fix typo

--- tests/Models/InventoryBucketTest.php
+++ tests/Models/InventoryBucketTest.php
@@ -28,18 +28,18 @@ class InventoryBucketTest extends \PHPUnit_Framework_TestCase
             ->setHash(953998645)
             ->setIndex(0);
 
-        $this->assertEquals(953998645, $bucket->getBucketHash();
-        $this->assertEquals('Heavy Weapons', $bucket->getBucketName();
-        $this->assertEquals('Machine guns and rocket launchers.', $bucket->getBucketDescription();
-        $this->assertEquals(0, $bucket->getScope();
-        $this->assertEquals(3, $bucket->getCategory();
-        $this->assertEquals(40, $bucket->getBucketOrder();
-        $this->assertEquals('BUCKET_HEAVY_WEAPON', $bucket->getBucketIdentifier();
-        $this->assertEquals(10, $bucket->getItemCount();
-        $this->assertEquals(1, $bucket->getLocation();
-        $this->assertEquals(true, $bucket->getHasTransferDestination();
-        $this->assertEquals(true, $bucket->getEnabled();
-        $this->assertEquals(953998645, $bucket->getHash();
-        $this->assertEquals(0, $bucket->getIndex();
+        $this->assertEquals(953998645, $bucket->getBucketHash());
+        $this->assertEquals('Heavy Weapons', $bucket->getBucketName());
+        $this->assertEquals('Machine guns and rocket launchers.', $bucket->getBucketDescription());
+        $this->assertEquals(0, $bucket->getScope());
+        $this->assertEquals(3, $bucket->getCategory());
+        $this->assertEquals(40, $bucket->getBucketOrder());
+        $this->assertEquals('BUCKET_HEAVY_WEAPON', $bucket->getBucketIdentifier());
+        $this->assertEquals(10, $bucket->getItemCount());
+        $this->assertEquals(1, $bucket->getLocation());
+        $this->assertEquals(true, $bucket->getHasTransferDestination());
+        $this->assertEquals(true, $bucket->getEnabled());
+        $this->assertEquals(953998645, $bucket->getHash());
+        $this->assertEquals(0, $bucket->getIndex());
     }
 }
